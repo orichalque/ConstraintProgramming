4946c350e95efe7cbfe2ec10a0649012992faf34
orichalque <thiblf@gmail.com>
2016-01-12 18:03:01 +0100

    interface problem cree

---
+++ problem.py
@@ -0,0 +1,23 @@
+# -*-coding:utf-8 -*
+
+class Problem:
+	domain = []
+	def __init__(self):	
+		print
+	
+	def initialNode(self):
+		print
+	
+	def testSat(self, node):
+		print
+	
+	def printSolution(self, node):
+		print "coucou"
+
+p = Problem()
+node = 0
+p.printSolution(node)
+
+
+
+	
