1448ee012d050e74f6b6fcf0d2add7506d97aa2e
benjimor <benji_moreau@hotmail.fr>
2016-02-07 18:14:18 +0100

    n queen problem file added

---
+++ Elisabeth/nQueenProblem.py
@@ -0,0 +1,23 @@
+# -*-coding:utf-8 -*
+import problem,
+
+class NQueenProblem(Problem):
+	__metaclass__ = ABCMeta
+	
+	domain = []
+	def __init__(self):	
+		return NotImplemented
+	
+	def initialNode(self):
+		return NotImplemented
+	
+	def testSat(self, node):
+		return NotImplemented		
+	
+	def printSolution(self, node):
+		return NotImplemented		
+
+
+
+
+	
