f7297c3cb9f4b736aaab1579c140eb7cfe2529a2
benjimor <benji_moreau@hotmail.fr>
2016-02-23 17:35:10 +0100

    testsat

--- Elisabeth/nQueenProblem.py
+++ Elisabeth/nQueenProblem.py
@@ -12,7 +12,25 @@ class NQueenProblem(Problem):
 		return NotImplemented
 	
 	def testSat(self, node):
-		return NotImplemented		
+		assigned = []
+		for clef in node.domains:
+			if len(node.domains[clef])==0:
+				return False
+			if len(node.domains[clef])==1:
+				assigned.append(node.domains[clef][0]
+			else
+				assigned.append(-1)
+		for i in range(0,len(assigned)):
+			if assigned[i] != -1:
+				for j in range(i+1,len(assigned)):
+					if assigned[j] != -1:
+						#test colonne
+						if assigned[i] == assigned[j]:
+							return False
+						#test diag
+						if j-i == abs(assigned[j]-assigned[i]):
+							return False
+		return True
 	
 	def printSolution(self, node):
 		return NotImplemented		
